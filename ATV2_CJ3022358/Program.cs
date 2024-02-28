f17d5d198f93161d44262f2c0e091c24f41968d6
kevpy1 <kevin.c@aluno.ifsp.edu.br>
2024-02-28 08:55:32 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2_CJ3022358/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ATV2_CJ3022358
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Per1//
+
+            float salario;
+
+            Console.WriteLine("Entre com o seu Salário:");
+            salario = float.Parse(Console.ReadLine());
+
+            salario = salario + (25.0f / 100.0f) * salario;
+
+            Console.WriteLine("O novo salário é R$ {0}", salario);
+
+
+        }
+    }
+}
+           //per2//
