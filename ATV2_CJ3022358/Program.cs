4025088fd48153ba90a8cbe18b37cec740a4af9d
kevpy1 <kevin.c@aluno.ifsp.edu.br>
2024-02-28 10:37:44 -0300

    exercicio

--- ATV2_CJ3022358/Program.cs
+++ ATV2_CJ3022358/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace ATV2_CJ3022358
+﻿namespace ATV2_cj3022358
+
 {
     internal class Program
+
     {
         static void Main(string[] args)
         {
-            //Per1//
+            //////////////////// EXERCICICO 1 ////////////////////
 
             float salario;
 
@@ -16,7 +18,49 @@
             Console.WriteLine("O novo salário é R$ {0}", salario);
 
 
+            //////////////////// EXERCICICO 2 ////////////////////
+
+            Console.WriteLine("Entre com o número:");
+            int n1 = int.Parse(Console.ReadLine());
+
+            int soma = (n1 * 3 + 1) + (n1 * 2 - 1);
+
+            Console.WriteLine("O número é: " + soma);
+
+
+
+
+
+
+
+
+
+
+
+
+            //////////////////// EXERCICICO 3 ///////////////////
+
+            float bolsa;
+
+            bolsa = 1000;
+
+            bolsa = bolsa - (bolsa / 1000) * 3.4f;
+            bolsa = bolsa + (bolsa / 1000) + 2.8f;
+            bolsa = bolsa + (bolsa / 1000) + 14;
+            bolsa = bolsa - (bolsa / 1000) - 8.5f;
+
+            Console.WriteLine("O valor do investimento é {0}", bolsa);
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
-           //per2//
